da8d6f75a6e72022d299a6908ed67b659c15b54a
Vadim Peretokin <vperetokin@gmail.com>
2019-09-30 07:04:28 +0000

    Add Russian translation so there's something to work with

--- static/i18n/ru.lua
+++ static/i18n/ru.lua
@@ -1,5 +1,10 @@
 return {
   ru = {
-    
+    upload_success = "Пакет %s загружен успешно",
+    greeting = "Здраствуйте, %s",
+    login_nav = "Вход",
+    register_nav = "Регистрация",
+    index_nav = "Индекс",
+    packages_nav = "Пакеты"
   }
 }
